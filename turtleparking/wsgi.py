a5cddcf00bb1b42aea39a74c31df5b8a153e0b49
Pedro Silva <pedro.silva@salesrabbit.com>
2025-07-15 18:32:43 +0100

    wsgi updated

--- turtleparking/wsgi.py
+++ turtleparking/wsgi.py
@@ -11,17 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB', 'turtleparking'),
-        'USER': os.environ.get('POSTGRES_USER', 'turtleparkinguser'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'yourpassword'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
-        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
-    }
-}
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'turtleparking.settings')
 
 application = get_wsgi_application()
